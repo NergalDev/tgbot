eb6aa1c3cdf7187485508a072ae7678891f60cc9
NergalDev <108280056+NergalDev@users.noreply.github.com>
2022-07-14 15:14:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+
+TOKEN = "5443998559:AAHYNCAjwKgbJZR3sAqTOuiBW5xvOhlOTRA"
---
+++ main.py
@@ -0,0 +1,48 @@
+import telebot
+from extencions import APIException, Converter
+from config import *
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Приветствие!"
+    bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['convert'])
+def values(message: telebot.types.Message):
+    text = 'Выберите валюту, из которой конвертировать:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, base_handler)
+
+def base_handler(message: telebot.types.Message):
+    base = message.text.strip()
+    text = 'Выберите валюту из которой конвертировать:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, sym_handler, base)
+
+def sym_handler(message: telebot.types.Message, base):
+    sym = message.text.strip()
+    text = 'Выберите количество конвертируемой валюты:'
+    bot.send_message(message.chat.id, text)
+    bot.register_next_step_handler(message, amount_handler, base, sym)
+
+def amount_handler(message: telebot.types.Message, base, sym):
+    amount = message.text.strip()
+    try:
+        new_price = Converter.get_price(base, sym, amount)
+    except APIException as e:
+        bot.send_message(message.chat.id, f"Ошибка в конвертации: \n{e} ")
+    else:
+        text = f"Цена {amount} {base} в {sym}: {new_price}"
+        bot.send_message(message.chat.id, text)
+
+bot.polling()
